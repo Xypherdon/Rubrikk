47ff3b77ae9b06dd9cda8a33cf4c72d4d3a674ce
Filip Rosian <rosian.filip@gmail.com>
2020-06-28 21:42:34 +0300

    Problem completed

---
+++ RubrikkTest/RubrikkTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubrikkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a command-line argument.");
+            }
+
+            string file = args[0];
+
+            bool order;
+            if (args[1] == "asc")
+            {
+                order = true;
+            } else if (args[1] == "desc")
+            {
+                order = false;
+            }
+            else
+            {
+                System.Console.WriteLine("Please enter a valid order");
+                return;
+            }
+
+            string type = args[2];
+
+            Sorter sorter = new Sorter();
+            sorter.ReadFromFile(file);
+
+            string finalString="";
+
+            if (type == "number")
+            {
+                List<int> sortedNumbers = sorter.NumberSort(order);
+                foreach(int number in sortedNumbers)
+                {
+                    finalString = finalString + number.ToString() + " ";
+                }
+            }else if (type == "string")
+            {
+                List<string> sortedNumbers = sorter.StringSort(order);
+                foreach(string number in sortedNumbers)
+                {
+                    finalString = finalString + number + " ";
+                }
+            }else if (type == "hybrid")
+            {
+                List<int> sortedNumbers = sorter.HybridSort(order);
+                foreach(int number in sortedNumbers)
+                {
+                    finalString = finalString + number.ToString() + " ";
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid type");
+                return;
+            }
+
+
+            Console.WriteLine(finalString);
+
+        }
+    }
+}
---
+++ RubrikkTest/RubrikkTest/Sorter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubrikkTest
+{
+    class Sorter
+    {
+        private List<int> numbers = new List<int>();
+        private List<string> stringNumbers = new List<string>();
+ 
+        public void ReadFromFile(string file)
+        {
+            string text;
+
+            try
+            {
+                text = System.IO.File.ReadAllText(@file);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            string[] splitText = text.Split(new string[] { ", " },StringSplitOptions.None);
+            
+            foreach(string number in splitText)
+            {
+                this.numbers.Add(Int32.Parse(number));
+                this.stringNumbers.Add(number);
+            }
+           
+        }
+
+        public List<int> NumberSort(bool order)
+        {
+            List<int> sortedList = this.numbers;
+            sortedList.Sort();
+            if (order)
+            {
+                return sortedList;
+            }
+            else
+            {
+                sortedList.Reverse();
+                return sortedList;
+            }
+        }
+
+        public List<string> StringSort(bool order)
+        {
+            List<string> sortedList = this.stringNumbers;
+            sortedList.Sort();
+            if (order)
+            {
+                return sortedList;
+            }
+            else
+            {
+                sortedList.Reverse();
+                return sortedList;
+            }
+        }
+
+        public List<int> HybridSort(bool order)
+        {
+            List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+
+            foreach(int number in this.numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    even.Add(number);
+                }
+                else
+                {
+                    odd.Add(number);
+                }
+            }
+
+            even.Sort();
+            odd.Sort();
+
+            if (order)
+            {
+                even.Reverse();
+                even.AddRange(odd);
+                return even;
+            }
+            else
+            {
+                odd.Reverse();
+                odd.AddRange(even);
+                return odd;
+            }
+        }
+    }
+}
